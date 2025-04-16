e5fea72645e58736e56fe1c1d9a9480ceab9302d
merkis88 <axuenno88@gmail.com>
2025-04-17 01:53:48 +0700

    CRUD

--- routes/telegraph.php
+++ routes/telegraph.php
@@ -1,10 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Log;
-use DefStudio\Telegraph\Models\TelegraphBot;
-use App\Http\Telegraph\Handler;
+//use DefStudio\Telegraph\Facades\Telegraph;
+//use App\Http\Telegraph\Handler;
 
-Route::telegraph()
-    ->token('7842446740:AAEJYeYBQZhEShnMKpPFqvHmYeLy9V9IBaw')
-    ->webhookHandler(Handler::class);
+
+//Telegraph::routes()
+  //  ->token(env('TELEGRAPH_BOT_TOKEN'))
+    //->webhookHandler(Handler::class);
